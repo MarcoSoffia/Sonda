24e73e70aded549064c1d70f74159fdff457b9c2
vDuB <atefjmai99@gmail.com>
2026-07-02 16:32:29 +0200

    Aggiunta la funzionalità di --help e altre modifiche nel main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,37 @@
 import Chunker
+import argparse
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-file_path = BASE_DIR / "requirements.txt"
+def helper() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+            prog="Sonda",
+            usage=argparse.SUPPRESS,
+            description="File exfiltration over network",
+            epilog="""
+usage: main.py -s file.txt -a 192.168.1.10
+usage: main.py -r file.pcap
+            """,
+            formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("-s", "--send", metavar="FILE", help="Send a file over a network protocol")
+    parser.add_argument("-r", "--read", metavar="FILE", help="Read a pcap file")
 
-# Read, Bytes
-with open(file_path, "rb") as f:
-    file = f.read()
+    return parser 
 
-chunker = Chunker.Chunker(file)
+if __name__ == "__main__":
+    BASE_DIR = Path(__file__).resolve().parent
+    
+    parser = helper()
+    args = parser.parse_args()
 
-chunks = chunker.chunk()
+    file_path = BASE_DIR / args.read
 
-[print(chunk) for chunk in chunks]
+    # Read, Bytes
+    with open(file_path, "rb") as f:
+        file = f.read()
+
+    chunker = Chunker.Chunker(file)
+    chunks = chunker.chunk()
+
+    [print(chunk) for chunk in chunks]
 
