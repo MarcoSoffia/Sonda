ff2383ddad56b39de65c3d1c08263f3e30edd88b
vvvDuB <atefjmai99@gmail.com>
2026-07-04 20:30:00 +0200

    Implementazione corretta della classe Frame

--- src/frame.py
+++ src/frame.py
@@ -1,11 +1,69 @@
+from hashlib import sha256
+from abc import ABC, abstractmethod
 
-class Frame:
-    TYPE_META = 0x00
-    TYPE_DATA = 0x01
+class Frame(ABC):
+    CODE_HASH = 0x00
+    CODE_DATA = 0x01
 
-    def __init__(self, type: int, payload: bytes, ):
-        self.type = type
+    def __init__(self, frame_type: int):
+        self.type = frame_type    
+    
+    @property
+    def type(self) -> int:
+        return self.__type
+
+    @type.setter
+    def type(self, value: int):
+        if isinstance(value, int) and value in (self.CODE_HASH, self.CODE_DATA):
+            self.__type = value
+        else:
+            raise TypeError("Invalid frame type")
+
+    @abstractmethod
+    def serialize(self) -> str:
+        pass
+
+
+class DataFrame(Frame):
+    def __init__(self, payload: bytes):
+        super().__init__(Frame.CODE_DATA)
         self.payload = payload
 
-    def serialize(self) -> bytes:
-        return self.type.to_bytes(1, 'big') + self.payload
+    @property
+    def payload(self) -> bytes:
+        return self.__payload
+
+    @payload.setter
+    def payload(self, value: bytes):
+        if isinstance(value, bytes) and value != b"":
+            self.__payload = list(value)
+        else:
+            raise TypeError("payload must be non-empty bytes")
+
+    def serialize(self) -> str:
+        return "".join(str(x) for x in list(self.payload))
+
+
+class HashFrame(Frame):
+    def __init__(self, file: bytes):
+        super().__init__(Frame.CODE_HASH)
+        self.file = file
+
+    @property
+    def file(self) -> bytes:
+        return self.__file
+
+    @file.setter
+    def file(self, file: bytes):
+        if isinstance(file, bytes) and file != b"":
+            self.__file = file
+        else:
+            raise TypeError("file must be non-empty bytes")
+
+    def serialize(self) -> str:
+        if not isinstance(self.file, bytes):
+            raise TypeError("file must be bytes")
+        
+        h256 = sha256()
+        h256.update(self.file)
+        return h256.hexdigest()
--- src/main.py
+++ src/main.py
@@ -1,23 +1,48 @@
+import frame
 import chunker
 import argparse
 from pathlib import Path
 
 def helper() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-            prog="Sonda",
-            usage=argparse.SUPPRESS,
-            description="File exfiltration over network",
-            epilog="""
+        prog="Sonda",
+        usage=argparse.SUPPRESS,
+        description="File transfer utility",
+        epilog="""
 usage: main.py -s file.txt -a 192.168.1.10
 usage: main.py -r file.pcap
-            """,
-            formatter_class=argparse.RawDescriptionHelpFormatter
+        """,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
     )
-    parser.add_argument("-s", "--send", metavar="FILE", help="Send a file over a network protocol")
-    parser.add_argument("-r", "--read", metavar="FILE", help="Read a pcap file")
-    parser.add_argument("-st", "--strategy", help="Select a strategy for sending the file")
 
-    return parser 
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "-s",
+        "--send",
+        metavar="FILE",
+        help="Send a file over a network protocol",
+    )
+    group.add_argument(
+        "-r",
+        "--read",
+        metavar="FILE",
+        help="Read a file",
+    )
+
+    parser.add_argument(
+        "-a",
+        "--address",
+        metavar="ADDRESS",
+        help="Destination address",
+    )
+
+    parser.add_argument(
+        "-st",
+        "--strategy",
+        help="Select a strategy for sending the file",
+    )
+
+    return parser
 
 if __name__ == "__main__":
     BASE_DIR = Path(__file__).resolve().parent
@@ -25,14 +50,23 @@ if __name__ == "__main__":
     parser = helper()
     args = parser.parse_args()
 
-    file_path = BASE_DIR / args.read
+    file = args.read or args.send
+    file_path = BASE_DIR / file
+
+    if not file_path.exists():
+        print(f"File {file_path} does not exist")
+        exit(1)
 
     # Read, Bytes
     with open(file_path, "rb") as f:
         file = f.read()
 
-    chunker = chunker.Chunker(file)
+    chunker = chunker.Chunker(file, 12)
     chunks = chunker.chunk()
 
-    [print(chunk) for chunk in chunks]
-
+    hash_file = frame.HashFrame(file) # sha256 del documento
+    
+    print(hash_file.serialize())
+    for chunk in chunks:
+        payload = frame.DataFrame(chunk)
+        print(payload.serialize()) # Questo valore stara' nel campo Payload
