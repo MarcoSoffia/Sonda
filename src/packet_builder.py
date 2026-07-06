ee482e92076231ba77756e44bee49f56434cbd98
hb <marco.soffia17@gmail.com>
2026-07-06 18:31:52 +0200

    packet_builder.py espanso in una classe più completa

--- src/packet_builder.py
+++ src/packet_builder.py
@@ -1,19 +1,45 @@
 """
-Function that builds a packet
+Function that builds the packets
 """
 
 import scapy.all as scapy
 from frame import HashFrame, DataFrame, Frame
 
 class PacketBuilder:
-    @staticmethod
-    def build_packet(payload: bytes, seq: int):
+    def __init__(self,ip: str, id: int):
+        self.ip = ip
+        self.id = id
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self,value: int):
+        if isinstance(value, int):
+            self.__id = value
+        else:
+            raise TypeError("id must be a integer")
+
+    @property
+    def ip(self):
+        return self.__ip
+
+    @ip.setter
+    def ip(self,value: str):
+        if isinstance(value, str):
+            self.__ip = value
+        else:
+            raise TypeError("ip must be a string")
+
+
+    def build_packet(self,payload: bytes, seq: int):
         if not isinstance(payload, bytes):
             raise TypeError("Payload must be a bytes")
 
         pkt = (
-                scapy.IP(dst="172.20.129.149")
-                / scapy.ICMP(id=333, seq=seq)
+                scapy.IP(dst=self.ip)
+                / scapy.ICMP(id=self.id, seq=seq)
                 / scapy.Raw(load=payload)
         )
         return pkt
